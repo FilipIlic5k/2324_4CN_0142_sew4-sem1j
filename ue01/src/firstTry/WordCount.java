c3fff9121196af8c34fe844f62bbe0bceb272218
Filip Ilic <filipilic@MacBook-Pro-von-Filip.local>
2023-09-18 16:57:49 +0200

    easyHTML, hardHTML, invalidHTML pass

--- ue01/src/firstTry/WordCount.java
+++ ue01/src/firstTry/WordCount.java
@@ -6,28 +6,28 @@ import java.util.List;
 
 public class WordCount {
     public static int count(String data){
-        data = data.trim();
         if(data.isEmpty()){
             return 0;
         }
 
-        //System.out.println("Before: " + data);
+        System.out.println("Before: " + data);
+        data = data.replaceAll("<(\"[^\"]*\"|'[^']*'|[^'\">])*>", " ");
+        data = data.replaceAll("<[^>]*$", " ");
         data = data.replaceAll("[^a-zA-Z]", " ");
-        //System.out.println("After: " + data);
-
-        String[] wordsRaw = data.split("\\s+");
-
+        data = data.trim();
+        System.out.println("After: " + data);
 
         List<String> words = new ArrayList<>(
-                Arrays.asList(wordsRaw)
+                Arrays.asList(data.split("\\s+"))
         );
 
         words.removeIf(String::isEmpty);
         words.removeIf(s -> s.matches("[^a-zA-Z]"));
 
-        //System.out.println(words);
+        System.out.println(words);
 
 
         return words.size();
     }
+
 }
--- ue01/src/firstTry/WordCountTest.java
+++ ue01/src/firstTry/WordCountTest.java
@@ -34,5 +34,51 @@ class WordCountTest {
         assertEquals(3, count(" ein  erster Text      "));
         assertEquals(3, count("ein:erster.Text"));
     }
+    
+   @Test
+   void easyHTML(){
+        assertEquals(1, count(" eins  <html> "));
+        assertEquals(1, count(" eins  < html> "));
+        assertEquals(1, count(" eins  <html > "));
+        assertEquals(1, count(" eins  < html > "));
+        assertEquals(4, count(" eins <html> zwei<html>drei <html> vier"));
+
+        assertEquals(2, count(" eins <html> zwei "));
+        assertEquals(2, count(" eins <html>zwei "));
+        assertEquals(2, count(" eins<html> zwei "));
+        assertEquals(2, count(" eins<html>zwei "));
+        assertEquals(2, count(" eins<img alt=\\\"xxx\\\" > zwei"));
+
+        assertEquals(2, count(" eins \\\"zwei\\\" "));
+        assertEquals(2, count(" eins\\\"zwei\\\" "));
+        assertEquals(2, count(" eins \\\"zwei\\\""));
+        assertEquals(3, count(" eins \\\"zwei\\\"drei"));
+   }
+    @Test
+    void hardHTML(){
+        assertEquals(2, count(" eins<img alt=\"<bild>\" > zwei"));
+        assertEquals(2, count(" eins<img alt=\\\"bild>\\\" > zwei"));
+        assertEquals(2, count(" eins<img alt=\\\"<bild>\\\" keinwort> zwei"));
+        assertEquals(2, count(" eins<img alt=\\\"<bild>\\\" src=\\\"bild.png\\\" >zwei"));
+        assertEquals(2, count(" eins<img alt=\\\"<bild\\\" keinwort>zwei"));
+
+        assertEquals(1, count(" eins<img alt=\\\"<bild\\\" keinwort"));
+        assertEquals(2, count(" eins<img alt=\\\"<bild\\\" keinwort> zwei"));
+        assertEquals(1, count(" eins<img alt=\\\"<bild keinwort> keinwort"));
+        assertEquals(2, count(" eins<img alt=\\\"<bild keinwort keinwort\\\">zwei"));
+        assertEquals(2, count(" eins<img alt=\\\"<bild keinwort< keinwort\\\">zwei"));
+    }
+
+    @Test
+    void insaneHardHTML(){
+        assertEquals(2, count(" eins<img alt=\\\"<bild \\\\\\\" keinwort> keinwort\\\" keinwort>zwei"));
+        assertEquals(2, count(" eins<img alt=\\\"<bild \\\\\\\" keinwort<keinwort\\\" keinwort>zwei"));
+        assertEquals(2, count(" eins<img alt=\\\"<bild \\\\\\\" keinwort keinwort\\\" keinwort>zwei"));
+    }
+
+    @Test
+    void invalidHTML(){
+        assertEquals(1, count(" eins<html"));
+    }
 
 }
